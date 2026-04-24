b8733dca7d8f7821aa7093e1037aa3d49514fd36
Lubeth <lubethjimenez@yahoo.com>
2026-04-24 18:50:48 -0400

    Add a useNavigate hook example DEMO

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,22 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
 export default function Home() {
+  const [login, setLogin] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    if(login){
+      navigate("/profile")
+    }
+  }, [login])
+
   return(
     <div>
-      Home
+      <button onClick={()=> {
+        setLogin(true)
+      }}>Login</button>
     </div>
   )
 }
