7ad500766dc3f7c54a43b4a9c635bd74039c7956
Lubeth <lubethjimenez@yahoo.com>
2026-04-23 21:52:20 -0400

    Add pages & nav bar to each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import { Routes } from 'react-router'
 import './App.css'
+import NavBar from './NavBar'
 
 function App() {
   
 
   return (
     <>
-    
+    <NavBar/>
     </>
   )
 }
---
+++ src/NavBar.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import { Link } from 'react-router';
+
+export default function NavBar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/sales">Sales</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
